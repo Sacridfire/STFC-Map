656d20a30ebe460f49280f0deb66c4bd36b538ba
Sacridfire <cody.cornmesser@yahoo.com>
2020-04-21 15:35:44 -0500

    its definitely a function

--- assets/js/scripts/Scripts.js
+++ assets/js/scripts/Scripts.js
@@ -1,3 +1,18 @@
+  // initialize the map
+	var map = L.map('map', {crs: L.CRS.Simple, maxZoom:10, minZoom: -2}).setView([-4679,-426]);
+	var bounds = [[-2238,-2358], [1462,-6458]];
+	var image = L.imageOverlay('assets/baked-map.png', bounds).addTo(map);
+	map.fitBounds(bounds);
+	
+	var tooltipThreshold = 2;
+		map.on('zoomend', function() {
+			if (map.getZoom() < tooltipThreshold) {
+			$(".leaflet-tooltip").css("display","none")
+			} else { 
+			$(".leaflet-tooltip").css("display","block")
+			}
+});
+
     // load Travel Paths
   $.getJSON("assets/json/travel-paths.geojson",function(data){
     // add GeoJSON layer to the map once the file is loaded
